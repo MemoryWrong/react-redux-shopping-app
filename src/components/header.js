df08c1523627587371ebd8d35e383c7d45a1711f
carl.lee <carl.lee@oakton.com.au>
2018-03-06 13:29:30 +1000

    refactoring the code

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from '../assets/logo.svg';
+import '../style/App.css';
+
+class HeaderComp extends Component {
+  render() {
+    return (
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          {/* <h1 className="App-title">Welcome to React</h1> */}
+          
+        </header>
+       
+    );
+  }
+}
+
+export default HeaderComp;
